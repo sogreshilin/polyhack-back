5b0b5818b8255a3323658cee3a3f34b991b5b5e8
Ivan Milyaev <i-milyaev@yandex-team.ru>
2019-07-07 14:13:28 +0700

    VIDEOINDEX-09: add uri decode

--- video-index/src/main/java/ru/nsu/fit/supernova/controller/SearchController.java
+++ video-index/src/main/java/ru/nsu/fit/supernova/controller/SearchController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriUtils;
 import ru.nsu.fit.supernova.model.Indexing.SearchResult;
 import ru.nsu.fit.supernova.service.index.VideoIndexService;
 
@@ -20,6 +21,7 @@ public class SearchController {
         @RequestParam(name = "query") String query,
         @RequestParam(name = "id", required = false) Long videoId
     ) {
+        query = UriUtils.decode(query, "UTF-8");
         List<String> tokens = Lists.newArrayList(query.split(" "));
         if (videoId == null) {
             return indexService.findByTokens(tokens);
